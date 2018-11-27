5330dd063f7943bd63c5d0389fca1919c2e5c012
jmanuel-si <45397401+jmanuel-si@users.noreply.github.com>
2018-11-27 17:01:09 +0100

    Initial Sub

---
+++ edt/staff/_genre.js
@@ -0,0 +1,10 @@
+﻿var libChoixGenre = "Recherche par :";
+var listeGenres = new Array ()
+listeGenres [0] = new GenreRessource ("grProf","PROFESSEURS");
+listeGenres [1] = new GenreRessource ("grClasse","CLASSES");
+listeGenres [2] = new GenreRessource ("grSalle","SALLES");
+
+function GenreRessource (aGenre, aLibelle) {
+  this.genre   = aGenre;
+  this.libelle = aLibelle;
+}
